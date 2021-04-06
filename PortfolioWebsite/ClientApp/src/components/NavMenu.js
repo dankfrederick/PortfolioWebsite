3eabf228fc15209032b0f4747571460fe29552ec
Dan Frederick <dan.k.frederick@outlook.com>
2021-04-06 15:48:49 -0700

    Updates to CSS and Nav Menu

--- PortfolioWebsite/ClientApp/src/components/NavMenu.js
+++ PortfolioWebsite/ClientApp/src/components/NavMenu.js
@@ -26,19 +26,23 @@ export class NavMenu extends Component {
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
-            <NavbarBrand tag={Link} to="/">PortfolioWebsite</NavbarBrand>
+            <NavbarBrand tag={Link} to="/" className="text-light"><strong>DanFrederick.me</strong></NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="text-light" to="/">Home</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                  <NavLink tag={Link} className="text-light" to="/pomodoro">Pomodoro Timer</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-light" to="/UNISWAP">UNISWAP</NavLink>
                 </NavItem>
+                  <NavItem>
+                   <NavLink tag={Link} className="text-light" to="/contact">Contact</NavLink>
+                  </NavItem>
+
               </ul>
             </Collapse>
           </Container>
